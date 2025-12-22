01965fb7d889d3e1ba12df37989721078a1b6162
Raghvendra Yadav <raghvendrayadavsms@gmail.com>
2025-12-22 22:01:17 +0530

    update origin

--- jobportal/src/main/java/com/portal/jobportal/config/CorsConfig.java
+++ jobportal/src/main/java/com/portal/jobportal/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://job-portal-frontend-7bxv8dnjw.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
